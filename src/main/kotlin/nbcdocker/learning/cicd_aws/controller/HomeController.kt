8349ddfd54676641b9ea7e0a445f334dedc84090
huouvcti <huouvcti@gamil.com>
2025-07-03 11:35:37 +0900

    env 추가

--- src/main/kotlin/nbcdocker/learning/cicd_aws/controller/HomeController.kt
+++ src/main/kotlin/nbcdocker/learning/cicd_aws/controller/HomeController.kt
@@ -1,14 +1,17 @@
 package nbcdocker.learning.cicd_aws.controller
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HomeController {
+class HomeController(
+    @Value("\${DB_USERNAME}") private val username: String,
+) {
 
     @GetMapping("/")
     fun home(): String {
-        return "home-aws"
+        return "home-aws $username"
     }
 
     @GetMapping("/healthz")
